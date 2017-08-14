8730db37820eff44d759c70249433858967bee8e
vishwanath K. Shetty <vishwanathshettyk@excelindia.com>
2017-08-14 10:06:40 +0530

    Test Case : Selecting Manage Course Material in a Section

---
+++ PegasusAutomationTestScripts/Pegasus Test Steps/CourseMaterials Steps/ManageCourseMaterailsSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using Pegasus.Pages.Pegasus_Modules.Course_Materails_Page;
+
+namespace PegasusAutomationTestScripts.Pegasus_Test_Steps.CourseMaterials_Steps
+{
+ [Binding]
+   public sealed class ManageCourseMaterailsSteps : ManageCourseMaterialsPage
+
+    {
+     [Given(@"I have clicked on ""(.*)"" Page")]
+     public void GivenIHaveClickedOnPage(string tabName)
+     {
+         base.SelectCourseMaterailPage(tabName);
+     }
+
+     [Given(@"I navigate to Wizard and Click on ""(.*)"" tile")]
+     public void GivenINavigateToWizardAndClickOnTile(string tileName)
+     {
+         base.AddAssignmentsInWizard(tileName);
+     }
+
+
+    }
+}
